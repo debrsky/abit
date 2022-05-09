bd9b9254615c0ed63304da0ae13b54a7d75aae21
debrsky <debrsky@gmail.com>
2022-05-09 11:15:59 +1000

    some work

---
+++ src/js/abits.js
@@ -0,0 +1,64 @@
+import {createAbitViewElem} from './abit-view.js';
+import PouchDB from 'pouchdb';
+const db = new PouchDB('my_database');
+
+const outputElem = document.getElementById('abit-list');
+const progressElem = document.getElementById('progress');
+
+outputElem.addEventListener('dblclick', (event) => {
+  const target = event.target.closest('.item');
+  console.log(target);
+  target.classList.toggle('dark');
+});
+
+outputElem.addEventListener('click', (event) => {
+  const target = event.target;
+  console.log(target);
+});
+
+let items = [];
+
+(async () => {
+  console.log('query');
+  console.time('0');
+  const dbDocs = await db.query('abits', {
+    include_docs: true,
+    attachments: true
+  });
+
+  console.log(dbDocs);
+
+  console.timeEnd('0');
+
+  const abits = dbDocs.rows.map((row) => row.doc);
+
+  console.time('1');
+  items = abits
+    .sort((a, b) => {
+      const a0 = a.fio;
+      const b0 = b.fio;
+      if (a0 < b0) return -1;
+      if (a0 > b0) return 1;
+      return 0;
+    })
+    .map((abit) => createAbitViewElem(abit));
+
+  progressElem.max = items.length - 1;
+  progressElem.value = 0;
+  const chunkSize = 150;
+  let idx = 0;
+  while (idx <= items.length) {
+    const chunk = items.slice(idx, idx + chunkSize);
+    outputElem.append(...chunk);
+
+    await new Promise((resolve) => setTimeout(resolve));
+    // await new Promise((resolve) => queueMicrotask(resolve));
+
+    idx = idx + chunkSize;
+    progressElem.value = idx;
+  }
+
+  console.timeEnd('1');
+})();
+
+console.log('It works.');
