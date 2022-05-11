42f033fa462fe3951296228e473318f6140ae95f
debrsky <debrsky@gmail.com>
2022-05-11 23:12:38 +1000

    fix memory leak

--- src/js/abit-view.js
+++ src/js/abit-view.js
@@ -3,5 +3,7 @@ import tmpl from '../pug/includes/abit-view.pug';
 export function createAbitViewElem(data) {
   const div = document.createElement('div');
   div.innerHTML = tmpl({data});
-  return div.firstElementChild;
+  const elem = div.firstElementChild;
+  div.remove();
+  return elem;
 }
