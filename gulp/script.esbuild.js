746573a3831d2ffb41aafc595fcf37865655d1e1
debrsky <debrsky@gmail.com>
2022-05-14 11:25:02 +1000

    add svelte loader to esbuild

--- gulp/script.esbuild.js
+++ gulp/script.esbuild.js
@@ -2,7 +2,9 @@ import gulp from 'gulp';
 import gulpEsbuild from 'gulp-esbuild';
 import {commonjs} from '@hyrious/esbuild-plugin-commonjs';
 import pug from 'pug';
+import {compile as svelteCompile} from 'svelte/compiler';
 import fs from 'fs';
+import path from 'path';
 
 const pugPlugin = () => ({
   name: 'pug',
@@ -17,6 +19,45 @@ const pugPlugin = () => ({
   }
 });
 
+// https://esbuild.github.io/plugins/#svelte-plugin
+const sveltePlugin = () => ({
+  name: 'svelte',
+  setup(build) {
+    build.onLoad({filter: /\.svelte$/}, async (args) => {
+      // This converts a message in Svelte's format to esbuild's format
+      const convertMessage = ({message, start, end}) => {
+        let location;
+        if (start && end) {
+          const lineText = source.split(/\r\n|\r|\n/g)[start.line - 1];
+          const lineEnd =
+            start.line === end.line ? end.column : lineText.length;
+          location = {
+            file: filename,
+            line: start.line,
+            column: start.column,
+            length: lineEnd - start.column,
+            lineText
+          };
+        }
+        return {text: message, location};
+      };
+
+      // Load the file from the file system
+      const source = await fs.promises.readFile(args.path, 'utf8');
+      const filename = path.relative(process.cwd(), args.path);
+
+      // Convert Svelte syntax to JavaScript
+      try {
+        const {js, warnings} = svelteCompile(source, {filename});
+        const contents = js.code + `//# sourceMappingURL=` + js.map.toUrl();
+        return {contents, warnings: warnings.map(convertMessage)};
+      } catch (e) {
+        return {errors: [convertMessage(e)]};
+      }
+    });
+  }
+});
+
 export default function script() {
   return gulp
     .src('src/js/*.js')
@@ -27,7 +68,7 @@ export default function script() {
         minify: false,
         define: {global: 'window'},
         sourcemap: 'linked',
-        plugins: [commonjs(), pugPlugin()]
+        plugins: [commonjs(), pugPlugin(), sveltePlugin()]
       })
     )
     .pipe(gulp.dest('./public/js'));
---
+++ src/js/abit-form.js
@@ -0,0 +1,6 @@
+import FormSvelte from './svelte/comp.svelte';
+
+const svelte = document.getElementById('svelte');
+const app = new FormSvelte({
+  target: svelte
+});
